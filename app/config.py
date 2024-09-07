f1bcd2b1963cc123fc69b6ba28c639f865d34025
symonmuchemi <simonkenya846@gmail.com>
2024-09-07 15:48:26 +0300

    init: set up configurations

---
+++ app/config.py
@@ -0,0 +1,32 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'AbrAcadabr@'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'Production': ProductionConfig,
+    'testing': TestingConfig,
+
+    'Default': DevelopmentConfig
+}
