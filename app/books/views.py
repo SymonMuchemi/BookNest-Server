99caee43a50bd4e3230aa0d74b5e53225fdf3b67
symonmuchemi <simonkenya846@gmail.com>
2024-09-08 14:29:35 +0300

    feat: created update view function

--- app/books/views.py
+++ app/books/views.py
@@ -5,6 +5,10 @@ from ..schema import BookSchema
 from app import db
 from app.books import books_bp
 
+book_error_dict = {
+    'Error': 'Could not find book'
+}
+
 
 @books_bp.route('/hello', methods=['GET'])
 def hello_world():
@@ -31,7 +35,7 @@ def create_book():
         book = Book(
             name=book_schema.name.lower(),
             author=book_schema.author.lower(),
-            count=book_schema.count,
+            quantity=book_schema.quantity,
             image_url=book_schema.image_url,
             rental_fee=book_schema.rental_fee
         )
@@ -71,18 +75,23 @@ def get_by_name(name):
             'name': book.name,
             'author': book.author,
             'image_url': book.image_url,
-            'count': book.count,
+            'quantity': book.quantity,
             'rental_fee': book.rental_fee
         } for book in books])
     
-    else:
-        return jsonify({
-            'Error': 'Could not find book'
-        }), 400
+    return book_error_dict, 400
 
 
 @books_bp.route('/get_by_author/<string:name>')
 def get_by_author(name):
+    """Gets a list of books from an author.
+
+    Args:
+        name (str): Name of the author.
+
+    Returns:
+        list: List of books.
+    """
     query_author = name.lower().replace('_', ' ')
     
     books = Book.query.filter_by(author=query_author).all()
@@ -93,10 +102,40 @@ def get_by_author(name):
             'name': book.name,
             'author': book.author,
             'image_url': book.image_url,
-            'count': book.count,
+            'quantity': book.quantity,
             'rental_fee': book.rental_fee
         } for book in books])
     
-    return jsonify({
-        'Error': 'Could not find book'
-    }), 400
+    return book_error_dict, 400
+
+
+@books_bp.route('/update/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book_data = request.json
+    
+    book = Book.query.get(book_id)
+
+    if book is None:
+        return book_error_dict, 400
+    
+    try:
+        book_schema = BookSchema(**book_data)
+        
+        book.name = book_schema.name
+        book.author = book_schema.author
+        book.image_url = book_schema.image_url
+        book.quantity = book_schema.quantity
+        book.rental_fee = book_schema.rental_fee
+        
+        db.session.commit()
+        
+        return jsonify({
+            "Message": "Book update successfully",
+            "New book": book_schema.model_dump()
+        })
+
+    except ValidationError as e:
+        return jsonify({
+            'Error': 'Cannot update book',
+            'Details': e.errors()
+        }), 400
