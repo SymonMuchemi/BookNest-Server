0fe6598f298ae02f338212c9913ce60d2e69f20a
symonmuchemi <simonkenya846@gmail.com>
2024-09-09 00:53:24 +0300

    feat: updates a member object
    creates the update_member view function to update a member with a given id

--- app/members/views.py
+++ app/members/views.py
@@ -15,15 +15,25 @@ members_error_dict = {
 
 @members_bp.route('/hello')
 def hello():
+    """Returns a string.
+
+    Returns:
+        str: Welcome string.
+    """
     return "Hello from the members side"
 
 
 @members_bp.route('/create', methods=['POST'])
 def create_member():
+    """Creates a member object and saves to database.
+
+    Returns:
+        dict: Response message.
+    """
     try:
-        member_data = request.json
+        member = request.json
         
-        member_schema = MemberSchema(**member_data)
+        member_schema = MemberSchema(**member)
         
         member = Member(
             name = member_schema.name,
@@ -47,4 +57,42 @@ def create_member():
     except IntegrityError as e:
         return jsonify({
             'Error': 'Name already exists',
+        })
+
+
+@members_bp.route('/update/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    """Updates a member object.
+
+    Args:
+        member_id (int): Id of the member object.
+
+    Returns:
+        dict: Response message.
+    """
+    member_data = request.json
+
+    member = Member.query.get(member_id)
+    
+    if member is None:
+        return members_error_dict, 400
+    
+    try:
+        member_schema = MemberSchema(**member_data)
+        
+        member.name = member_schema.name
+        member.debt = member_schema.debt
+        
+        db.session.commit()
+        
+        return jsonify({
+            'Message': "Member updated successfully!",
+            'New member': member_schema.model_dump()
+        })
+        
+
+    except ValidationError as e:
+        return jsonify({
+            'Error': 'Cannot update member',
+            'Details': e.errors()
         })
