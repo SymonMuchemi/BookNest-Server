414a0cf7657cb1a12fa64f3f221f0025a7ea1ec2
symonmuchemi <simonkenya846@gmail.com>
2024-09-09 20:41:31 +0300

    update: adds book.quantity as a requirement

--- app/schema.py
+++ app/schema.py
@@ -7,16 +7,13 @@ class BookSchema(BaseModel):
     name: str = Field(..., min_length=3, max_length=255)
     author: str = Field(..., min_length=3, max_length=255)
     image_url: str = Field(..., min_length=10)
-    quantity: int = Field(gt=0)
-    rental_fee: int = Field(gt=10)
+    quantity: int = Field(..., gt=0)
+    rental_fee: int = Field(gt=10, default=10)
 
 
 class MemberSchema(BaseModel):
     name: str = Field(..., min_length=3, max_length=255)
     debt: int = Field(..., ge=0, lt=500)
-
-
-
 
 
 class TransactionSchema(BaseModel):
