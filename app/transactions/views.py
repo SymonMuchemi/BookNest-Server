3874c705161c1311f2737ef07fa4a0d21282d53a
symonmuchemi <simonkenya846@gmail.com>
2024-09-09 17:59:29 +0300

    feat: implements a create view function

--- app/transactions/views.py
+++ app/transactions/views.py
@@ -1,6 +1,10 @@
 from app import db
 from app.transactions import transactions_bp
-from flask import jsonify
+from flask import jsonify, request
+from pydantic import ValidationError
+from ..utils import TransactionType
+from ..schema import TransactionSchema
+from ..models import Transaction, Member, Book
 
 
 @transactions_bp.route('/hello', methods=['GET'])
@@ -11,3 +15,62 @@ def hello_world():
         str: A hello string.
     """
     return 'Hello from transactions.'
+
+
+@transactions_bp.route('/create', methods=['POST', 'GET'])
+def create_transaction():
+    """Records a transaction and updates members.debt.
+
+    Returns:
+        dict: Response message.
+    """
+    data = request.json
+    
+    try:
+        transaction_schema = TransactionSchema(**data)
+        
+        member_id = transaction_schema.member_id
+        book_id = transaction_schema.book_id
+        transaction_type = transaction_schema.type
+        amount = transaction_schema.amount
+        
+        member = Member.query.get(member_id)
+        book = Book.query.get(book_id)
+        
+        if member is None or book is None:
+            return jsonify({
+                'Message': 'Cannot find member or book details'
+            }), 400
+        
+        if transaction_type == TransactionType.RETURN and member.debt > 0:
+            member.debt -= amount
+        if transaction_type == TransactionType.ISSUE and book.rental_fee > 0:
+            if member.debt + book.rental_fee > 500:
+                return jsonify({
+                    'Error': "Member's debt will exceed ksh 500"
+                }), 400
+
+            member.debt += book.rental_fee
+
+        transaction = Transaction(
+            book_id=book_id,
+            member_id=member_id,
+            type=transaction_type,
+            amount=amount,
+            date=transaction_schema.date
+        )
+
+        db.session.add(member)
+        db.session.add(transaction)
+        db.session.commit()
+        
+        return jsonify({
+            'Message': 'Transaction recorded succefully',
+            'transaction': transaction_schema.model_dump(),
+        }), 201
+
+    except ValidationError as e:
+        return jsonify({
+            'error': 'Validation failed',
+            'details': e.errors()
+        }), 400
