e0baada0bc1bf16a6dd3b2749b9d65c7078d05e1
symonmuchemi <simonkenya846@gmail.com>
2024-09-10 13:11:03 +0300

    update: renames book.rental_fee to book.penalty_fee

--- app/schema.py
+++ app/schema.py
@@ -8,7 +8,7 @@ class BookSchema(BaseModel):
     author: str = Field(..., min_length=3, max_length=255)
     image_url: str = Field(..., min_length=10)
     quantity: int = Field(..., gt=0)
-    rental_fee: int = Field(gt=10, default=10)
+    penalty_fee: int = Field(ge=10, default=10)
 
 
 class MemberSchema(BaseModel):
@@ -23,3 +23,8 @@ class TransactionSchema(BaseModel):
     type: TransactionType = Field(...)
     amount: int = Field(default=0)
     date: datetime = Field(default_factory=datetime.now)
+
+
+class BookRequestSchema(BaseModel):
+    member_id: int = Field(...)
+    book_id: int = Field(...)
