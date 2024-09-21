11ec27b4f7a51b4ed447534c8011fb106a178b71
symonmuchemi <simonkenya846@gmail.com>
2024-09-21 12:11:29 +0300

    refactor: changes transactions db model
    modifies transactions table to handle book issue and return on one page

--- app/models.py
+++ app/models.py
@@ -3,36 +3,34 @@ from datetime import datetime
 
 
 class Book(db.Model):
-    __tablename__ = 'books'
+    __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     author = db.Column(db.String(50))
     penalty_fee = db.Column(db.Integer, default=10)
     quantity = db.Column(db.Integer, nullable=False)
-    
-    transactions = db.relationship('Transaction', backref='book')
+
+    transactions = db.relationship("Transaction", backref="book")
 
 
 class Member(db.Model):
-    __tablename__ = 'members'
+    __tablename__ = "members"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     debt = db.Column(db.Integer, default=0)
     books_borrowed = db.Column(db.Integer, default=0)
-    
-    transactions = db.relationship('Transaction', backref='member')
+
+    transactions = db.relationship("Transaction", backref="member")
 
 
 class Transaction(db.Model):
-    __tablename__ = 'transactions'
+    __tablename__ = "transactions"
     id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
-    member_id = db.Column(db.Integer, db.ForeignKey('members.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"))
+    member_id = db.Column(db.Integer, db.ForeignKey("members.id"))
+    issued_on = db.Column(db.DateTime, default=datetime.now())
+    returned_on = db.Column(db.DateTime)
+    charge = db.Column(db.Integer, default=0)
     type = db.Column(
-        db.Enum(
-            'issue',
-            'return',
-            name='transaction_types'),
-        nullable=False
+        db.Enum("issue", "return", name="transaction_types"), nullable=False
     )
-    date = db.Column(db.DateTime, default=datetime.now())
