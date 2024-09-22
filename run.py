fa720ba9728b6aae6b6b059a0c6da5c7590309d6
symonmuchemi <simonkenya846@gmail.com>
2024-09-23 02:11:30 +0300

    specifies environment to use

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import os
 from app import create_app
 
-app = create_app()
+app = create_app(os.environ.get("FLASK_CONFIG") or "default")
 
-if __name__ == '__main__':
-    app.run(port='0.0.0.0')
+if __name__ == "__main__":
+    app.run(port="0.0.0.0")
