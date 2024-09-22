00f05ee6486727f501ec506d844e338cfc56be02
symonmuchemi <simonkenya846@gmail.com>
2024-09-23 02:26:59 +0300

    update: gets flask env not config

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import os
 from app import create_app
 
-app = create_app(os.environ.get("FLASK_CONFIG") or "default")
+app = create_app(os.environ.get("FLASK_ENV") or "default")
 
 if __name__ == "__main__":
     app.run(port="0.0.0.0")
